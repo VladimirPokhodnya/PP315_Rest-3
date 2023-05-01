5ed21ddbeebef87ca6a1216ea16f4ec48c56f184
hammondy <hammondy@student.21-school.ru>
2023-05-01 16:16:46 +0700

    rest admin

--- src/main/resources/static/admin.js
+++ src/main/resources/static/admin.js
@@ -1,23 +1,12 @@
 function userAuthInfo() {
-    // const tbody = document.getElementById('user-table');
     const panel = document.getElementById("user-header");
     fetch('http://localhost:8080/user/api')
         .then((res) => res.json())
         .then((userInfo) => {
-            let temp = '';
             let roles = [];
             for (let role of userInfo.roles) {
                 roles.push(" " + role.name.toString().substring(5))
             }
-            temp += `<tr>
-                        <td>${userInfo.id}</div>
-                        <td>${userInfo.firstname}</td>
-                        <td>${userInfo.lastname}</td>
-                        <td>${userInfo.age}</td>
-                        <td>${userInfo.username}</td>
-                        <td>${userInfo.role}</td>
-                    </tr>`;
-            // tbody.innerHTML = temp;
             panel.innerHTML = `<h5><b>${userInfo.username}</b> with roles: <span>${userInfo.role}</span></h5>`
         });
 }
