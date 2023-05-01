ecee506d005809a35fa85dbb138564d9181d077f
hammondy <hammondy@student.21-school.ru>
2023-05-01 23:54:27 +0700

    rest userAll

--- src/main/resources/static/admin.js
+++ src/main/resources/static/admin.js
@@ -12,3 +12,39 @@ function userAuthInfo() {
 }
 
 userAuthInfo()
+
+function usersAll() {
+    const tbody = document.getElementById("users-table")
+    fetch("http://localhost:8080/admin/api/users")
+        .then((res) => res.json())
+        .then((usersInfo) => {
+            let table_row = '';
+            let roles = [];
+            for (let us of usersInfo) {
+                for (let role of us.roles) {
+                    roles.push(" " + role.name.toString().substring(5))
+                }
+                table_row += `<tr>
+                        <td>${us.id}</td>
+                        <td>${us.firstname}</td>
+                        <td>${us.lastname}</td>
+                        <td>${us.age}</td>
+                        <td>${us.username}</td>
+                        <td>${us.role}</td>
+                        <td>
+                            <button href="#modalEdit${us.id}" type="button"
+                                    class="btn btn-info" data-toggle="modal">Edit
+                            </button>
+                        </td>
+
+                        <td>
+                            <button href="#modalDelete${us.id}" type="button"
+                                    class="btn btn-danger" data-toggle="modal">Delete
+                            </button>
+                        </td>
+                    </tr>`;
+            }
+            tbody.innerHTML = table_row;
+        });
+}
+usersAll()
