b90d8c4372993da51a09f6102d6bbbfc80897b4e
hammondy <hammondy@student.21-school.ru>
2023-05-02 07:49:04 +0700

    rest controllers

--- src/main/java/ru/kata/controller/AdminRestController.java
+++ src/main/java/ru/kata/controller/AdminRestController.java
@@ -1,5 +1,6 @@
 package ru.kata.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -9,22 +10,41 @@ import ru.kata.service.UserService;
 import java.util.List;
 
 @RestController
-@RequestMapping("/admin")
+@RequestMapping("/admin/api")
 public class AdminRestController {
     private final UserService userService;
 
+    @Autowired
     public AdminRestController(UserService userService) {
         this.userService = userService;
     }
 
-    @GetMapping("/api/users")
+    @GetMapping("/users")
     public ResponseEntity<List<User>> apiGetAllUsers() {
-        return  new ResponseEntity<>(userService.getAllUsers(), HttpStatus.OK);
+        return new ResponseEntity<>(userService.getAllUsers(), HttpStatus.OK);
     }
 
-    @GetMapping("/api/users/{id}")
+    @GetMapping("/users/{id}")
     public ResponseEntity<User> apiGetUserById(@PathVariable("id") Long id) {
         return new ResponseEntity<>(userService.findOne(id), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> apiCreateUser(@RequestBody User user) {
+        userService.saveUser(user);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @PutMapping("/users")
+    public ResponseEntity<Void> apiUpdateUser(@RequestBody User user) {
+        userService.saveUser(user);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @DeleteMapping("/users/{id}")
+    public ResponseEntity<Void> apiDeleteUser(@PathVariable("id") Long id) {
+        userService.delete(id);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
 }
